1092bbd29d8b06a60394a27e77ab8e53bdcd736c
Serhii-Yakimtso <serafyso@gmail.com>
2021-02-25 23:03:38 +0200

    profile added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Profile from './components/Profile/Profile'
+import Friendlist from './components/FriendList/FriendList'
+import Statistics from './components/Statistics/Statistics'
+import TransactionHistory from './components/TransactionHistory/TransactionHistory'
+
+import profileUsers from './db/user.json'
+
+console.log(profileUsers)
+
+const {name, tag, location, avatar, stats }=profileUsers
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <Profile name={name} tag={tag} location={location} avatar={avatar} stats={stats} />
+      <Friendlist />
+      <Statistics />
+      <TransactionHistory/>
+    </div>
+  );
+}
+
+export default App;
