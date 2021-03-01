74833451008e8282490abb575931d12d3ba2b9d2
Serhii-Yakimtso <serafyso@gmail.com>
2021-03-01 22:09:47 +0200

    TransactionHistory.css add (*.js - repair)

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -2,36 +2,34 @@ import React from "react";
 import ProtoTypes from "prop-types";
 import s from "./TransactionHistory.module.css";
 
-const TransactionHistory = (transaction) => {
+const TransactionHistory = ({ transaction }) => {
   console.log(transaction);
 
   return (
     <>
-      <section className={s.transactionHistory}>
-        <h2>TransactionHistory</h2>
-        <table className="transaction-history">
-          <thead>
-            <tr>
-              <th>Type</th>
-              <th>Amount</th>
-              <th>Currency</th>
-            </tr>
-          </thead>
+      <h2>TransactionHistory</h2>
+      <table className={s.table}>
+        <thead className={s.tableHead}>
+          <tr>
+            <th className={s.tableItem}>Type</th>
+            <th className={s.tableItem}>Amount</th>
+            <th className={s.tableItem}>Currency</th>
+          </tr>
+        </thead>
 
-          <tbody>
-            {transaction.map((el) => {
-              const { id, type, amount, currency } = el;
-              return (
-                <tr className={s.row} key={id}>
-                  <td>{type}</td>
-                  <td>{amount}</td>
-                  <td>{currency}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </section>
+        <tbody>
+          {transaction.map((el) => {
+            const { id, type, amount, currency } = el;
+            return (
+              <tr className={s.tableRow} key={id}>
+                <td className={s.tableItem}>{type}</td>
+                <td className={s.tableItem}>{amount}</td>
+                <td className={s.tableItem}>{currency}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
